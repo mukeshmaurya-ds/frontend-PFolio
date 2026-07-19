002862b00d3a48bec5fa634ba3ac5d0016c370e0
mukeshmaurya-ds <mukesh.maurya.dev@gamil.com>
2026-07-19 15:10:00 +0530

    Remove accidental nested client project

--- client/src/utils/format-date.ts
+++
@@ -1,9 +0,0 @@
-export function formatDate(
-  date: string | Date,
-  locale = "en-US"
-) {
-  return new Intl.DateTimeFormat(locale, {
-    year: "numeric",
-    month: "long",
-  }).format(new Date(date));
-}
